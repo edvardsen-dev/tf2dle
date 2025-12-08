a249447bf8348d63d12c1a77172d33c85284e0e4
Radek Titěra <50217333+Rodak123@users.noreply.github.com>
2025-12-08 12:45:06 +0100

    Add spy inspired 404 screen (#97)
    * Add the Spy 404 screen + add: pickRandom utility
    
    * fix: Speech bubble uses smaller text on smaller screens + fix: Spy speaking component never overflows
    
    * fix: Using the <Button/> element as a link
    
    * fix: Re-enabled random quotes
    
    * add: Patch note

---
+++ src/routes/+error.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import * as Card from '$lib/components/ui/card';
+	import WinterDecore from '$lib/features/theme/components/winter/WinterDecore.svelte';
+	
+    import { page } from '$app/state';
+	import { SpySpeaking } from '$lib/components/ui/spy-speaking';
+	import Button from '$lib/components/ui/button/button.svelte';
+
+    const status: number = page.status;
+    const error: App.Error | null | undefined = page.error;
+</script>
+
+<div class="width m-auto">
+    <Card.Root class="relative">
+        <Card.Header>
+            {#if status === 404}
+                <Card.Title>404</Card.Title>
+                <Card.Description>Not found </Card.Description>
+            {:else if error}
+                <Card.Title>Error {status}</Card.Title>
+                <Card.Description>{error.message ?? 'An unknown error occurred.'}</Card.Description>
+            {:else}
+                <Card.Title>Error {status}</Card.Title>
+                <Card.Description>An unexpected error occurred.</Card.Description>
+            {/if}
+        </Card.Header>
+        <Card.Content>
+            <WinterDecore />
+            <SpySpeaking />
+
+			<Button 
+                href={'/'} 
+                class="mb-2 w-full"
+                variant="secondary"
+            >
+                Abort!
+            </Button>
+        </Card.Content>
+    </Card.Root>
+</div>
+
+<style scoped>
+	.width {
+		width: min(100%, 700px);
+	}
+</style>
