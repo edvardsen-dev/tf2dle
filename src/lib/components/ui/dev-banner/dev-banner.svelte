bd5bcbd69a5d8650b743fa57a15c0e5b6387bcc6
jKm00 <joakimedvardsen2000@gmail.com>
2025-12-12 15:49:48 +0100

    fix: remove unused imports

--- src/lib/components/ui/dev-banner/dev-banner.svelte
+++ src/lib/components/ui/dev-banner/dev-banner.svelte
@@ -1,7 +1,3 @@
-<script lang="ts">
-	import { AlertCircle } from 'lucide-svelte';
-</script>
-
 {#if import.meta.env.DEV}
 	<p class="absolute bottom-0 right-0 px-4 pl-6 py-1 bg-primary rounded-tl-full">Development</p>
 {/if}
